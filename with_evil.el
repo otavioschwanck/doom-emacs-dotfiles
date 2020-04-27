c7a4a7e625fed32a3c8158622b888e5933964d23
Otávio Schwanck <otavioschwanck@gmail.com>
2020-04-27 12:49:45 -0300

    improve evil

--- with_evil.el
+++ with_evil.el
@@ -6,7 +6,34 @@
       "C-p" nil)
 
 (map! :map ruby-mode-map
-      "C-M-k" #'ruby-beginning-of-block
-      "C-M-j" #'ruby-end-of-block)
+      "C-k" #'ruby-beginning-of-block
+      "C-j" #'ruby-end-of-block)
+
+(map! :nomv "C-e" 'end-of-line)
 
 (map! :n "C-<SPC>" #'er/expand-region)
+
+(map! :i "<C-return>" #'+company/dabbrev)
+
+(map! :leader "ç" 'save-buffer)
+
+(map! :after company
+      :map company-active-map
+      "<tab>" #'yas-expand)
+
+(after! company
+  (defadvice! +company--abort-previous-a (&rest _)
+    :before #'company-begin-backend
+    (company-abort))
+
+  (setq company-dabbrev-downcase 0)
+  (setq company-idle-delay 0.15))
+
+(defun yas-next-and-close-company ()
+  (interactive)
+  (company-abort)
+  (yas-next-field))
+
+(map! :after yasnippet
+      :map yas-keymap
+      "<tab>" 'yas-next-and-close-company)
